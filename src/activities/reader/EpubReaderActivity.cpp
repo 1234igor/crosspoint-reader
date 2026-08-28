991ae1d90e59426ad2e1e0506add841a7fd35480
Julia Nguyen <julia@uxj.io>
2026-08-28 15:23:16 -0400

    feat: add clipping selection shortcut

--- src/activities/reader/EpubReaderActivity.cpp
+++ src/activities/reader/EpubReaderActivity.cpp
@@ -625,6 +625,9 @@ void EpubReaderActivity::loop() {
       case CrossPointSettings::LP_MENU_DICTIONARY:
         openDictionaryWordSelect();
         return;
+      case CrossPointSettings::LP_MENU_CREATE_CLIPPING:
+        startClipSelection();
+        return;
       case CrossPointSettings::LP_MENU_READER_MENU:
       case CrossPointSettings::LP_MENU_DISABLED:
       default:
@@ -652,6 +655,9 @@ void EpubReaderActivity::loop() {
         if (!showDictionaryMessage) {
           openDictionaryWordSelect();
         }
+        return;
+      case CrossPointSettings::LP_MENU_CREATE_CLIPPING:
+        startClipSelection();
         return;
       case CrossPointSettings::LP_MENU_READER_MENU:
         if (usesToolbarMenu() && section) {
@@ -1051,6 +1057,7 @@ unsigned long EpubReaderActivity::confirmLongPressThreshold() const {
   switch (SETTINGS.longPressMenuFunction) {
     case CrossPointSettings::LP_MENU_BOOKMARK:
     case CrossPointSettings::LP_MENU_DICTIONARY:
+    case CrossPointSettings::LP_MENU_CREATE_CLIPPING:
       return ReaderUtils::BOOKMARK_HOLD_MS;
     case CrossPointSettings::LP_MENU_KOSYNC:
       return KOREADER_STORE.hasCredentials() ? ReaderUtils::GO_HOME_MS : 0;
